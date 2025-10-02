e77250abe2a9d126ca2c3ba38ec06277b9a2c2dc
Gabrielle Ribeiro <gabriellerpadua@gmail.com>
2025-10-02 20:49:39 -0300

    Aplicação da documentação Swagger no Controller de alunos

--- app/Controllers/alunos.controller.py
+++ app/Controllers/alunos.controller.py
@@ -7,6 +7,71 @@ alunos_bp = Blueprint("alunos", __name__)
 
 @alunos_bp.route("/alunos", methods=["POST"])
 def criar_aluno():
+    
+    """
+    Cria um novo aluno
+    
+    tags:
+    - alunos
+    description: Cria um novo aluno com os dados fornecidos no corpo da requisição.
+    consumes:
+    - application/json
+    produces:
+    - application/json
+    parameters:
+        -in: body
+        name:aluno
+        description: Dados do aluno a ser criado
+        required: true
+        schema:
+            type: object
+            required:
+                - nome
+                - idade
+                - turma_id
+                - data_nascimento
+                - nota_semestre1
+                - nota_semestre2
+            properties:
+                nome:
+                    type: string
+                    example: Maria Silva
+                idade:
+                    type: integer
+                    example: 20
+                turma_id:
+                    type: integer
+                    example: 1
+                data_nascimento:
+                    type: string
+                    format: date
+                    example: 2005-05-15
+                nota_semestre1:
+                    type: number
+                    format: float
+                    example: 8.5
+                nota_semestre2:
+                    type: number
+                    format: float
+                    example: 5.0
+    responses:
+        200:
+            description: Aluno criado com sucesso
+            schema:
+                type: object
+                properties:
+                    message: Aluno criado com sucesso!
+                    example: {"message": "Aluno criado com sucesso!"}
+        400:
+            description: Erro ao criar aluno
+            schema:
+                type: object
+                properties:
+                    error:
+                        type: string
+                        example: Não foi possível cadastrar aluno. Verifique os dados fornecidos.
+    """
+
     try:
         data = request.get_json()
 
@@ -28,6 +93,60 @@ def criar_aluno():
 
 @alunos_bp.route("/alunos", methods=["GET"])
 def listar_alunos():
+    
+    """
+    Retorna uma lista com todos os alunos existentes.
+    
+    tags:
+    - alunos
+    description: Retorna uma lista com todos os alunos existentes.
+    produces:
+    - application/json
+    responses:
+        200:
+            description: Lista de alunos
+            schema:
+                type: array
+                items:
+                    type: object
+                    properties:
+                        id:
+                        type: integer
+                        example: 1
+                        nome:
+                            type: string
+                            example: Maria Silva
+                        idade:
+                            type: integer
+                            example: 20
+                        turma_id:
+                            type: integer
+                            example: 3039
+                        data_nascimento:
+                            type: string
+                            format: date
+                            example: 2001-01-11
+                        nota_semestre1:
+                            type: number
+                            format: float
+                            example: 8.25
+                        nota_semestre2:
+                            type: number
+                            format: float
+                            example: 1.5
+                        media_final:
+                            type: number
+                            format: float
+                            example: 4.875
+        400:
+            description: Erro ao tentar listar alunos
+            schema:
+                type: object
+                properties:
+                    error:
+                        type: string
+                        example: Não foi possível listar os alunos
+    """
     alunos = Aluno.query.all()
     resultado = []
     for aluno in alunos:
@@ -45,6 +164,65 @@ def listar_alunos():
 
 @alunos_bp.route("/alunos/<int:aluno_id>", methods=["GET"])
 def obter_aluno(aluno_id):
+
+    """
+    Retorna as informações de um aluno específico baseado em seu ID
+
+    tags:
+    - alunos
+    description: Retorna as informações de um aluno baseado em seu ID.
+    produces:
+    - application/json
+    parameters:
+        - in: path
+        name: aluno_id
+        description: ID do aluno que será usado para identificá-lo
+        required: true
+        type: integer
+    responses:
+        200:
+        description: Informações do aluno retornada com sucesso
+        schema:
+            type: object
+            properties:
+                id:
+                    type: integer
+                    example: 2403658
+                nome:
+                    type: string
+                    example: Maria Silva
+                idade:
+                    type: integer
+                    example: 25
+                turma_id:
+                    type: integer
+                    example: 3039
+                data_nascimento:
+                    type: string
+                    format: date
+                    example: 1998-03-22
+                nota_semestre1:
+                    type: number
+                    format: float
+                    example: 6.75
+                nota_semestre2:
+                    type: number
+                    format: float
+                    example: 4.0
+                media_final:
+                    type: number
+                    format: float
+                    example: 5.375
+        404:
+        description: Erro ao tentar obter informações do aluno especificado
+        schema:
+            type: object
+            properties:
+                error:
+                    type: string
+                    example: Aluno não encontrado. Favor verificar o ID informado.
+        """
+    
     aluno = Aluno.query.get(aluno_id)
     if not aluno:
         return jsonify({"error": "Aluno não encontrado"}), 404
@@ -62,6 +240,76 @@ def obter_aluno(aluno_id):
 
 @alunos_bp.route("/alunos/<int:aluno_id>", methods=["PUT"])
 def atualizar_aluno(aluno_id):
+    """
+    Atualiza as informações de um aluno com base em seu ID.
+
+    tags:
+    - alunos
+    description: Atualiza as informações de um aluno com base em seu ID.
+    consumes:
+    - application/json
+    produces:
+    - application/json
+    parameters:
+        - in: path
+        name: aluno_id
+        description: ID do aluno que terá dados atualizados
+        required: true
+        type: integer
+        - in: body
+        name: aluno
+        description: Dados do aluno que serão atualizados
+        required: true
+        schema:
+            type: object
+            properties:
+                nome:
+                    type: string
+                    example: Maria José
+                idade:
+                    type: integer
+                    example: 21
+                turma_id:
+                    type: integer
+                    example: 3039
+                data_nascimento:
+                    type: string
+                    format: date
+                    example: 2002-07-19
+                nota_semestre1:
+                    type: number
+                    format: float
+                    example: 7.0
+                nota_semestre2:
+                    type: number
+                    format: float
+                    example: 6.5
+    responses:
+        200:
+            description: Aluno atualizado com sucesso
+            schema:
+                type: object
+                properties:
+                    message:
+                        type: string
+                        example: Aluno atualizado com sucesso!
+        400:
+            description: Erro ao tentar atualizar o aluno
+            schema:
+                type: object
+                properties:
+                    error:
+                        type: string
+                        example: Não foi possível atualizar o aluno. Verifique os dados fornecidos.
+        404:
+            description: Aluno não encontrado para atualização
+            schema:
+                type: object
+                properties:
+                    error:
+                        type: string
+                        example: Aluno não encontrado
+    """
     aluno = Aluno.query.get(aluno_id)
     if not aluno:
         return jsonify({"error": "Aluno não encontrado"}), 404
@@ -89,12 +337,46 @@ def atualizar_aluno(aluno_id):
     
 @alunos_bp.route("/alunos/<int:aluno_id>", methods=["DELETE"])
 def deletar_aluno(aluno_id):
+    """
+    Exclui um aluno da base de dados baseado em seu ID.
+
+    tags:
+    - alunos
+    description: Exclui um aluno com base em seu ID.
+    produces:
+    - application/json
+    parameters:
+        - in: path
+        name: aluno_id
+        description: ID do aluno que será excluído da base de dados
+        required: true
+        type: integer
+    responses:
+        200:
+            description: Aluno deletado com sucesso
+            schema:
+                type: object
+                properties:
+                    message:
+                        type: string
+                        example: Aluno deletado com sucesso!
+        400:
+        404:
+            description: Aluno não encontrado para prosseguir com a exclusão
+            schema:
+                type: object
+                properties:
+                    error:
+                        type: string
+                        example: Aluno não encontrado       
+    """
     aluno = Aluno.query.get(aluno_id)
     if not aluno:
         return jsonify({"error": "Aluno não encontrado"}), 404
     db.session.delete(aluno)
     db.session.commit()
-    return jsonify({"message": "Aluno deletado com sucesso!"}), 200 
+    return jsonify({"message": "Aluno deletado com sucesso!"}), 200
+    
 
     
 
