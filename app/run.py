93573eb1569cdb89c2ede0c0125364286f45c069
nathaliafdsousa <nathalia.fdsousa@gmail.com>
2025-10-07 10:44:45 -0300

    Correção do Blueprint

--- app/run.py
+++ app/run.py
@@ -4,11 +4,11 @@ from flask import Flask, jsonify
 app = create_app()
 
 from app.Controllers.alunos_controller import alunos_bp
-from app.Controllers.professor_controller import professor_bp
+from app.Controllers.professor_controller import professores_bp
 from app.Controllers.turmas_controller import turmas_bp
 
 app.register_blueprint(alunos_bp)
-app.register_blueprint(professor_bp)
+app.register_blueprint(professores_bp)
 app.register_blueprint(turmas_bp)
 
 @app.route("/health")
