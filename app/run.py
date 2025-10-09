9b47cdae444df4f0393e63b736836afbafef01e4
Nathalia Ferreira <nathalia.fdsousa@gmail.com>
2025-10-09 08:28:50 -0300

    Correção de sintaxe

--- app/run.py
+++ app/run.py
@@ -12,4 +12,4 @@ def home():
     return jsonify({"message":"API Sistema Escolar rodando no container!"})
 
 if __name__ == "__main__":
-    app.run(debug=True) #debug=True ativa o modo debug e reinicia o servidor automaticamente ao detectar mudanças no código
+    app.run(debug=True)
